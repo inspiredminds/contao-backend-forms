460e4e6c7062ae19f05451b551dfd93a0e66f9c4
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2019-05-15 10:26:24 +0200

    add more back end forms elements

--- src/Form/BackendForm.php
+++ src/Form/BackendForm.php
@@ -13,14 +13,31 @@ class BackendForm extends \Haste\Form\Form
 
     public function addFormField($strName, array $arrDca, ArrayPosition $position = null)
     {
-        $type = $arrDca['inputType'];
+        self::adjustDcaForBackend($arrDca);
+
+        if (!is_array($arrDca['label'])) {
+            $arrDca['label'] = [$arrDca['label']];
+        }
+
+        return parent::addFormField($strName, $arrDca, $position);
+    }
+
+    public function addFieldFromFormGenerator($strName, array $arrDca, ArrayPosition $position = null)
+    {
+        self::adjustDcaForBackend($arrDca);
+
+        parent::addFieldFromFormGenerator($strName, $arrDca, $position);
+    }
+
+    protected static function adjustDcaForBackend(array &$arrDca): void
+    {
+        $type = $arrDca['inputType'] ?: $arrDca['type'];
 
         $arrDca['eval']['template'] = $arrDca['eval']['template'] ?? 'form_' . $type . '_backend';
+        $arrDca['template'] = $arrDca['template'] ?? 'form_' . $type . '_backend';
 
         if ('submit' === $type) {
             $arrDca['eval']['class'] = $arrDca['eval']['class'] ?? 'tl_submit';
         }
-
-        return parent::addFormField($strName, $arrDca, $position);
     }
 }
