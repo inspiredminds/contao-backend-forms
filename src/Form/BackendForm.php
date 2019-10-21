13ef4c1a5175bb5d641c9cc81429599112552726
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2019-10-21 16:32:50 +0100

    add default fieldset

--- src/Form/BackendForm.php
+++ src/Form/BackendForm.php
@@ -1,48 +1,63 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the ContaoBackendFormsBundle.
+ *
+ * (c) inspiredminds
+ *
+ * @license LGPL-3.0-or-later
+ */
+
 namespace InspiredMinds\ContaoBackendFormsBundle\Form;
 
+use Contao\FrontendTemplate;
+use Contao\TemplateLoader;
 use Haste\Util\ArrayPosition;
 
 class BackendForm extends \Haste\Form\Form
 {
+    protected $legend;
+
     public function generate($templateName = null)
     {
-        return parent::generate($templateName ?? 'form_backend');
+        if (null === $templateName) {
+            $templateName = 'form_backend';
+
+            try {
+                TemplateLoader::getPath($templateName, 'html5');
+            } catch (\Exception $e) {
+                $templateName = 'form_wrapper';
+            }
+        }
+
+        $template = new FrontendTemplate($templateName);
+        $template->legend = $this->legend;
+        $template->class = 'hasteform_'.$this->getFormId();
+        $template->formSubmit = $this->getFormId();
+
+        $this->addToTemplate($template);
+
+        return $template->parse();
     }
 
     public function addFormField($strName, array $arrDca, ArrayPosition $position = null)
     {
         self::adjustDcaForBackend($arrDca);
 
-        if (!is_array($arrDca['label'])) {
+        if (!\is_array($arrDca['label'])) {
             $arrDca['label'] = [$arrDca['label']];
         }
 
         return parent::addFormField($strName, $arrDca, $position);
     }
 
-    public function addFieldFromFormGenerator($strName, array $arrDca, ArrayPosition $position = null)
+    public function addFieldFromFormGenerator($strName, array $arrDca, ArrayPosition $position = null): void
     {
         self::adjustDcaForBackend($arrDca);
 
         parent::addFieldFromFormGenerator($strName, $arrDca, $position);
-    }
-
-    protected static function adjustDcaForBackend(array &$arrDca): void
-    {
-        $type = $arrDca['inputType'] ?: $arrDca['type'];
-        $class = $GLOBALS['TL_FFL'][$type];
-
-        /** @var \Contao\Widget $widget */
-        $widget = new $class;
-
-        $arrDca['eval']['template'] = $arrDca['eval']['template'] ?? $widget->template.'_backend';
-        $arrDca['template'] = $arrDca['template'] ?? $widget->template.'_backend';
-
-        if ('submit' === $type) {
-            $arrDca['eval']['class'] = $arrDca['eval']['class'] ?? 'tl_submit';
-        }
     }
 
     public function addToObject($objObject)
@@ -59,5 +74,28 @@ class BackendForm extends \Haste\Form\Form
         }
 
         return $this;
+    }
+
+    public function setLegend(string $legend): self
+    {
+        $this->legend = $legend;
+
+        return $this;
+    }
+
+    protected static function adjustDcaForBackend(array &$arrDca): void
+    {
+        $type = $arrDca['inputType'] ?: $arrDca['type'];
+        $class = $GLOBALS['TL_FFL'][$type];
+
+        /** @var \Contao\Widget $widget */
+        $widget = new $class();
+
+        $arrDca['eval']['template'] = $arrDca['eval']['template'] ?? $widget->template.'_backend';
+        $arrDca['template'] = $arrDca['template'] ?? $widget->template.'_backend';
+
+        if ('submit' === $type) {
+            $arrDca['eval']['class'] = $arrDca['eval']['class'] ?? 'tl_submit';
+        }
     }
 }
