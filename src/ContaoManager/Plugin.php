2cb5256324d79ed14eb77997f61561d97dd5ed0e
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2025-02-23 19:41:29 +0000

    fix namespace

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -6,12 +6,13 @@ declare(strict_types=1);
  * (c) INSPIRED MINDS
  */
 
-namespace InspiredMinds\ContaoBackendFormsBundle;
+namespace InspiredMinds\ContaoBackendFormsBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use InspiredMinds\ContaoBackendFormsBundle\ContaoBackendFormsBundle;
 
 /**
  * Plugin for the Contao Manager.
